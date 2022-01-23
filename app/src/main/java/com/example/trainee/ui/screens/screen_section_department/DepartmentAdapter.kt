7ccc9a0aa2f549497750e412b12746a148ac1f4e
lolketch <lightning110299@gmail.com>
2022-01-23 19:59:20 +1000

    Adapter changed to ListAdapter

---
+++ app/src/main/java/com/example/trainee/ui/screens/screen_section_department/DepartmentAdapter.kt
@@ -0,0 +1,65 @@
+package com.example.trainee.ui.screens.screen_section_department
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.example.trainee.R
+import com.example.trainee.data.model.User
+
+interface UserListAdapterClicks {
+    fun onItemClick(model: User)
+}
+
+class DepartmentAdapter : ListAdapter<User, RecyclerView.ViewHolder>(UserItemDiffCallback()) {
+    private var userListAdapterClicks: UserListAdapterClicks? = null
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val viewHolder = holder as DepartmentAdapter.DefaultViewHolder
+        viewHolder.nameUserTextView.text =
+            currentList[position].firstName + " " + currentList[position].lastName
+        viewHolder.positionUserTextView.text = currentList[position].position
+        viewHolder.tagUserTextView.text = currentList[position].userTag.toLowerCase()
+        Glide
+            .with(viewHolder.itemView)
+            .asBitmap()
+            .fitCenter()
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
+            .load(currentList[position].avatar)
+            .placeholder(R.drawable.ic_face_90)
+            .into(viewHolder.avatar)
+
+        viewHolder.itemView.setOnClickListener {
+            userListAdapterClicks?.onItemClick(currentList[position])
+        }
+    }
+
+    fun attachClicks(userListAdapterClicks: UserListAdapterClicks) {
+        this.userListAdapterClicks = userListAdapterClicks
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return DefaultViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item, parent, false)
+        )
+    }
+
+    inner class DefaultViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val avatar: ImageView = itemView.findViewById(R.id.avatarImageView)
+        val nameUserTextView: TextView = itemView.findViewById(R.id.nameUserTextView)
+        val positionUserTextView: TextView = itemView.findViewById(R.id.positionUserTextView)
+        val tagUserTextView: TextView = itemView.findViewById(R.id.tagUserTextView)
+    }
+}
+
+class UserItemDiffCallback : DiffUtil.ItemCallback<User>() {
+    override fun areItemsTheSame(oldItem: User, newItem: User): Boolean = oldItem == newItem
+
+    override fun areContentsTheSame(oldItem: User, newItem: User): Boolean = oldItem == newItem
+}
--- app/src/main/java/com/example/trainee/ui/screens/screen_section_department/DepartmentFragment.kt
+++ app/src/main/java/com/example/trainee/ui/screens/screen_section_department/DepartmentFragment.kt
@@ -1,7 +1,6 @@
 package com.example.trainee.ui.screens.screen_section_department
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
@@ -12,7 +11,6 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.trainee.R
 import com.example.trainee.base.App
 import com.example.trainee.data.model.User
-import com.example.trainee.data.model.Users
 import com.example.trainee.ui.screens.screen_profile.ProfileFragment
 import com.example.trainee.utils.MultiViewModelFactory
 import com.example.trainee.utils.SearchListener
@@ -24,7 +22,7 @@ class DepartmentFragment : Fragment(R.layout.fragment_department), SearchListene
     lateinit var departmentViewModel: DepartmentViewModel
     private var nameDepartment: String? = null
     private lateinit var recyclerView: RecyclerView
-    private lateinit var adapter: TestAdapter
+    private lateinit var adapter: DepartmentAdapter
     private val listUsers: MutableList<User> = mutableListOf()
 
     companion object {
@@ -42,7 +40,7 @@ class DepartmentFragment : Fragment(R.layout.fragment_department), SearchListene
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         recyclerView = view.findViewById(R.id.departmentRecyclerView)
-        adapter = TestAdapter()
+        adapter = DepartmentAdapter()
         recyclerView.layoutManager = LinearLayoutManager(context)
         recyclerView.adapter = adapter
         observeListUsers(adapter)
@@ -58,20 +56,20 @@ class DepartmentFragment : Fragment(R.layout.fragment_department), SearchListene
         })
     }
 
-    private fun observeSearchList(adapter: TestAdapter) {
+    private fun observeSearchList(adapter: DepartmentAdapter) {
         departmentViewModel.searchList.observe(viewLifecycleOwner, { listUsers ->
-            adapter.addData(listUsers)
+            adapter.submitList(listUsers)
         })
     }
 
-    private fun observeListUsers(adapter: TestAdapter) {
+    private fun observeListUsers(adapter: DepartmentAdapter) {
         departmentViewModel.listUsers.observe(viewLifecycleOwner, {
             if (nameDepartment == "all") {
                 listUsers.addAll(it.items)
-                adapter.addData(listUsers)
+                adapter.submitList(listUsers)
             } else {
                 listUsers.addAll(it.items.filter { user -> user.department == nameDepartment })
-                adapter.addData(listUsers)
+                adapter.submitList(listUsers)
             }
         })
     }
